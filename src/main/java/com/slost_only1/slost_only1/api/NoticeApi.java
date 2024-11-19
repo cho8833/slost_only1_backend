85c4528add4c463979af761d75d6afa9c4bdb23c
cho8833 <chowsp1009@gmail.com>
2024-11-19 18:19:36 +0900

    notice api

---
+++ src/main/java/com/slost_only1/slost_only1/api/NoticeApi.java
@@ -0,0 +1,29 @@
+package com.slost_only1.slost_only1.api;
+
+import com.slost_only1.slost_only1.config.response.Response;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("notice")
+public class NoticeApi {
+
+    @GetMapping("/announcement")
+    public Response<NoticeRes> getAnnouncementUrl() {
+        return new Response<>(new NoticeRes("https://yellow-beginner-483.notion.site/13ffda39711380f6b122e068a57ad15c?pvs=4"));
+    }
+
+    @GetMapping("/faq")
+    public Response<NoticeRes> getFAQUrl() {
+        return new Response<>(new NoticeRes("https://yellow-beginner-483.notion.site/13ffda3971138090b9d3d77b29af09d8?pvs=4"));
+    }
+
+    @GetMapping("/policy")
+    public Response<NoticeRes> getPolicyUrl() {
+        return new Response<>(new NoticeRes("https://yellow-beginner-483.notion.site/13ffda39711380728923fd11310af8c9?pvs=4"));
+    }
+
+    public record NoticeRes(String url) {
+    }
+}
